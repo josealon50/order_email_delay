281757d7eab2f872395f01e362f955555378358d
josealon50 <josel4404@gmail.com>
2022-02-01 00:45:24 -0800

    Included config and autoload script

--- order_email_delay.php
+++ order_email_delay.php
@@ -1,6 +1,19 @@
 <?php
-    
+/**
+ * * Order Email Delay: 
+ * * Arguments: 
+ * * Out: CSVs generated files
+ * *-------------------------------------------------------------------------------------------------------------------------------------
+ * * 01/11/21   JL  Created Script
+ * *
+ * *
+***/
+    include_once('../config.php');
+    include_once('autoload.php');
 
+    date_default_timezone_set('America/Los_Angeles');
+
+    global $appconfig, $logger;
 
 ?>
 
